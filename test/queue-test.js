f11f64c1685160aadc6c7df516c68c7ebf51b7e4
Chris Allen <crallend@gmail.com>
2014-05-30 09:13:33 -0500

    Removed queue auto-start behavior

--- test/queue-test.js
+++ test/queue-test.js
@@ -3,44 +3,31 @@ var util = require('util');
 var Queue = require('../lib/queue');
 var Job = require('../lib/job');
 
-function TestJob(id) {
-  Job.apply(this, [id]);
-}
-util.inherits(TestJob, Job);
-
-TestJob.prototype.run = function(callback) {
-  var id = this.id;
-  setTimeout(function() {
-    callback && callback({ id: id });
-  }, 5);
-};
-
 describe('Queue', function() {
-  describe('#add', function() {
-
-    it('starts running as soon as a job is added', function(done) {
-      var queue = new Queue(1);
-      queue.on('jobDone', function(result) {
-        assert.equal(1, result.id);
-        done();
-      });
-      var job = new TestJob(1);
-      queue.add(job);
+  
+  it('raises job done event when each job is finished', function(done) {
+    var queue = new Queue(1);
+    queue.on('jobDone', function(result) {
+      assert.equal(1, result.id);
+      done();
     });
-
-    it('raises complete event when queue is finished', function(done) {
-      var queue = new Queue(1);
-      queue.on('complete', function(results) {
-        for(var i in results) {
-          assert.equal(i, results[i].id);
-        }
-        done();
-      });
-      for(var i = 1; i <= 10; i++) {
-        var job = new TestJob(i);
-        queue.add(job);
-      }
-    });
-
+    queue.add(new Job(1));
+    queue.start();
   });
+
+  it('raises complete event when queue is finished', function(done) {
+    var queue = new Queue(1);
+    queue.on('complete', function(results) {
+      for(var i in results) {
+        assert.equal(i, results[i].id);
+      }
+      done();
+    });
+    for(var i = 1; i <= 10; i++) {
+      var job = new Job(i);
+      queue.add(job);
+    }
+    queue.start();
+  });
+
 });
--- test/request-job-test.js
+++ test/request-job-test.js
@@ -5,27 +5,25 @@ var RequestJob = require('../lib/request-job');
 var localhost = nock('https://localhost/api/v1/products');
 
 describe('RequestJob', function() {
-  describe('#run', function() {
 
-    it('should parse the uri', function() {
-      var job = new RequestJob(1, {
-        uri: 'https://localhost/api/v1/products'
-      });
-      assert.equal('localhost', job.host);
-      assert.equal('/api/v1/products', job.path);
+  it('should parse the uri', function() {
+    var job = new RequestJob(1, {
+      uri: 'https://localhost/api/v1/products'
     });
-
-    it('should populate the result with request timing and status', function() {
-      var job = new RequestJob(1, {
-        uri: 'https://localhost/api/v1/products'
-      });
-      job.run(function(result) {
-        assert(result.duration);
-        assert.equal(200, result.status);
-        assert.equal(1, result.id);
-        done();
-      });
-    });
-
+    assert.equal('localhost', job.host);
+    assert.equal('/api/v1/products', job.path);
   });
+
+  it('should populate the result with request timing and status', function() {
+    var job = new RequestJob(1, {
+      uri: 'https://localhost/api/v1/products'
+    });
+    job.run(function(result) {
+      assert(result.duration);
+      assert.equal(200, result.status);
+      assert.equal(1, result.id);
+      done();
+    });
+  });
+
 });
